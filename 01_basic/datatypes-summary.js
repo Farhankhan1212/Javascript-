8b1e936a462c079d79f34ad6918a1e118f161078
MD FARHAN KHAN <139714280+Farhankhan1212@users.noreply.github.com>
2024-02-24 20:04:23 +0000

    datatype summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,30 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined , BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234567888n    
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "farhan",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
